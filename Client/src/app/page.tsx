21dd222b87d77daf5bae6d45da2656fe4fc79c33
Dhyey-MedKart <dhyey.s@medkart.in>
2025-01-21 10:39:54 +0530

    F2

--- Client/src/app/page.tsx
+++ Client/src/app/page.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react'
-import Test from '@/components/test';
-import Link from 'next/link';
-
+import Test from '../components/test'
 function page() {
     return (
-        <div className="flex flex-col font-pop items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
-            <Test />
-
-        </div>
+        <Test/>
     );
 }
 
--- Client/src/components/test.tsx
+++ Client/src/components/test.tsx
@@ -1,48 +1,16 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { useToast } from '@chakra-ui/react'
-import { Button } from '@chakra-ui/react'
-export function Test() {
-  const toast = useToast()
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+
+const Test: React.FC = () => {
+    const router = useRouter();
+
+
   return (
-    <Button
-      onClick={() =>
-        toast({
-          title: 'Account created.',
-          description: "We've created your account for you.",
-          status: 'success',
-          duration: 9000,
-          isClosable: true,
-          
-        })
-      }
-    >
-      Show Toast
-    </Button>
-  )
-}
+    router.push('/login')
+  );
+};
 
-export default function PromiseBasedToastExample() {
-  const toast = useToast()
-  return (
-    <Button
-      onClick={() => {
-        // Create an example promise that resolves in 5s
-        const examplePromise = new Promise((resolve, reject) => {
-          setTimeout(() => resolve(200), 5000)
-        })
-
-        // Will display the loading toast until the promise is either resolved
-        // or rejected.
-        toast.promise(examplePromise, {
-          success: { title: 'Promise resolved', description: 'Looks great' },
-          error: { title: 'Promise rejected', description: 'Something wrong' },
-          loading: { title: 'Promise pending', description: 'Please wait' },
-        })
-      }}
-    >
-      Show Toast
-    </Button>
-  )
-}
+export default Test;
